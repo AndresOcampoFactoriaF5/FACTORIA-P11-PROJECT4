70ca0c1f7f5e313a083dfc802d9e941e21c6477a
AndresOcampoFactoriaF5 <less.broken.hearts@outlook.com>
2023-10-20 13:41:53 +0200

    presentación

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
+  import DivHightlighted from "./components/DivHightlighted.svelte";
   import Header from "./components/Header.svelte";
   import MovieList from "./components/MovieList.svelte";
 </script>
 
 <Header/>
+<DivHightlighted/>
 <MovieList/>
---
+++ src/components/DivHightlighted.svelte
@@ -0,0 +1,82 @@
+<script>
+
+</script>
+
+<div class="textBox">
+    <h1>
+        Descubre el cine que lo cambia todo
+    </h1>
+    <p>
+        Con nuestra suscripción, accede a obras maestras, clásicos, cine independiente, series exclusivas, documentales y mucho más.
+    </p>
+    <div>
+        <label for="email">Para iniciar tu suscripción, introduce tu email aquí
+            <input  class="email" id="email" type="email" placeholder="Tu Email">
+        </label>
+
+    </div>
+</div>
+
+<style>
+    h1, p {
+        color: #FFFFFF;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 64px;
+        margin: 64px 0px 24px;
+        line-height: 70px;
+        margin-bottom: 1.5rem;
+        margin-top: 4rem;
+        font-weight: 900;
+    }
+
+    p {
+        padding: 0px 16px;
+        font-size: 24px;
+        margin: 0px 85px 48px;
+        height: 72px;
+        font-weight: bold;
+        
+    }
+
+    .textBox {
+        position: relative;
+        text-align: center;
+        z-index: 1;
+        line-height: 1.5;
+        font-size: 1rem;
+        height: 100vh;
+    }
+
+    label {
+        color:#ffffff;
+        font-size: 18px;
+        font-weight: 550;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;    
+        margin: 0.5rem 0;
+    }
+    
+    #email {
+        border-radius: 4px;
+        border: 2px;
+        color:#FFFFFF;
+        border-color: #FFFFFF;
+        background-color:#060613;
+        width: 450px;
+        height: 48px;
+        border: 2px solid #6F6F81;
+        padding-left: 16px;
+        align-items: center;
+    }
+    #email:hover {
+        border: 2px solid #5EFFC3;
+    }
+</style>
+
+
+
